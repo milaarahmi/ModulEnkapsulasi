0e82632e9dd8c6da6262b07ea41693cc7195e2d5
Mila <Mila@192.168.56.1>
2017-08-29 13:02:46 -0700

    ModulEnkapsulasi

---
+++ src/LATIHAN3/TestSiswa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LATIHAN3;
+
+/**
+ *
+ * @author Mila
+ */
+public class TestSiswa {
+    public static void main(String args[]) {
+        EncapSiswa siswa= new EncapSiswa();
+        siswa.setName("Puspita");
+        siswa.setAbsen(30);
+        siswa.setAddress("Malang");
+        
+        System.out.print(" Name : "+siswa.getName()+ " Absen : "+siswa.getAbsen()
+         +" Address : " +siswa.getAddress()); 
+    }
+}
